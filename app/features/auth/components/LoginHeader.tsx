6d21ae758a58b96abc18c51c9d4f44322095c2db
jsanch387 <jesuss387@gmail.com>
2025-01-19 15:45:16 -0600

    fix error handling and text sans

--- app/features/auth/components/LoginHeader.tsx
+++ app/features/auth/components/LoginHeader.tsx
@@ -3,7 +3,7 @@
 export function LoginHeader() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl sm:text-4xl lg:text-5xl text-primary font-bold">
+      <h1 className="text-3xl sm:text-4xl lg:text-5xl text-primary font-sans font-black">
         Welcome Back
       </h1>
     </div>
