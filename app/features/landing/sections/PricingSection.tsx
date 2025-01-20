7613fbdc9a0b9520bc386ba07bdc7f51685dccdf
jsanch387 <jesuss387@gmail.com>
2025-01-20 14:42:35 -0600

    add section helper text

--- app/features/landing/sections/PricingSection.tsx
+++ app/features/landing/sections/PricingSection.tsx
@@ -13,9 +13,13 @@ export default function PricingSection() {
   return (
     <section id="pricing" className="py-16">
       <div className="max-w-7xl mx-auto px-6 sm:px-8">
-        <h2 className="text-3xl sm:text-4xl font-sans font-black text-center mb-12">
+        <h2 className="text-3xl sm:text-4xl font-sans font-black text-center mb-4">
           Plans that work for you
         </h2>
+        <p className="text-center text-lg text-text-secondary mb-12">
+          Generating high-quality content costs money, but we’ve priced our
+          plans to be affordable and accessible for everyone.
+        </p>
         <PricingCards cardVariant="outline" onButtonClick={handleClick} />
       </div>
     </section>
