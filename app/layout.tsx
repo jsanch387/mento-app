fe391bcc7a39511bef1c3ee3d803bf86dc0991c9
jsanch387 <jesuss387@gmail.com>
2025-02-24 19:22:11 -0600

    FB Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
+import Script from "next/script"; // Import next/script
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -54,6 +55,36 @@ export default function RootLayout({
           })}
         </script>
       </Head>
+
+      {/* Meta Pixel Code - OUTSIDE Head */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1164238438526373'); 
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1164238438526373&ev=PageView&noscript=1"
+          alt="" // Decorative image for accessibility
+        />
+      </noscript>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
